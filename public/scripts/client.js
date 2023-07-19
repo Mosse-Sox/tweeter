e2d3cb320606dc053ed64710f57fc24c013a263c
Mosse-Sox <autumngreenie990@gmail.com>
2023-07-19 21:10:53 +0000

    adds tweets to the page, ready to connect to database

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,34 +4,41 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const createTweetElement = function(tweetData) {
+const createTweetElement = function (tweet) {
   const $tweet = `<article>
   <header>
     <div>
       <img src="https://i.imgur.com/73hZDYK.png" />
-      <h4>${tweetData.user.name}</h4>
+      <h4>${tweet.user.name}</h4>
     </div>
-    <p>${tweetData.user.handle}</p>
+    <p>${tweet.user.handle}</p>
   </header>
 
-  <p class="tweet">${tweetData.content.text}</p>
+  <p class="tweet">${tweet.content.text}</p>
 
   <hr />
   <footer>
-    <p>${tweetData.created_at}</p>
+    <p>${tweet.created_at}</p>
     <div>
       <i class="fa-solid fa-flag"></i>
       <i class="fa-solid fa-retweet"></i>
       <i class="fa-solid fa-heart"></i>
     </div>
   </footer>
-</article>`
+</article>`;
 
   return $tweet;
-}
+};
 
-$(document).ready(function () {
-  const tweetData = {
+const renderTweets = function (container, tweets) {
+  for (const tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    container.append($tweet);
+  }
+};
+
+const tweets = [
+  {
     user: {
       name: "Newton",
       avatars: "https://i.imgur.com/73hZDYK.png",
@@ -40,12 +47,24 @@ $(document).ready(function () {
     content: {
       text: "If I have seen further it is by standing on the shoulders of giants",
     },
-    created_at: 1461116232227,
-  };
+    created_at: 1689623163705,
+  },
+  {
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
+    },
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1689709563705,
+  },
+];
 
-  const $tweet = createTweetElement(tweetData);
+$(document).ready(function () {
+  const $container = $("#tweets-container");
 
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $("#tweets-container").append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  renderTweets($container, tweets);
 });
